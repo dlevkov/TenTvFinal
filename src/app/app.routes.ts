3c8a026f2119dc93e228cccc6f8d996bf4fba58f
antonk <antonk@nana10.tv>
2017-02-14 12:28:03 +0200

    app route test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,7 @@ export const ROUTES: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: 'main', component: MainComponent },
   { path: 'mainfiltered/:id', component: MainComponent },
+  { path: 'mainfiltered', redirectTo: '/main', pathMatch: 'full' },
   { path: 'article-list', component: ArticlesListComponent },
   { path: 'Section/:id', component: SectionComponent },
   { path: 'section/:id', component: SectionComponent },
