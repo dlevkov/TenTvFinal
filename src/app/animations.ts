1764579f8f4451e8becb0e022e0a5bbf3986be83
Denis Levkov <denisl@nana10.tv>
2017-02-27 17:06:55 +0200

    Animation moved to separate file

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -3,10 +3,33 @@ import { trigger, state, style, transition, animate } from '@angular/core';
 export const pageTransition =
     trigger('transition', [
 
-        state('in', style({ transform: 'translateY(0)', opacity: '0' })),
+        state('in', style({
+            transform: 'translateY(0)',
+            opacity: '0'
+        })),
 
         transition('void => *', [
             style({ transform: 'translateY(-15px)', opacity: '1' }),
             animate(1000)
         ])
     ]);
+export const visibilityChanged = trigger('visibilityChanged', [
+    state('shown', style({
+        opacity: 1,
+        transform: 'translateY(0)'
+    })),
+    state('hidden', style({
+        opacity: 0,
+        transform: 'translateY(-100%)'
+    })),
+    transition('shown => hidden', animate('400ms')),
+    transition('hidden => shown', animate('300ms'))
+]);
+export const confirmationAnimation = trigger(
+    'confirmationAnimation', [
+        transition(':enter', [
+            style({ transform: 'translateY(-100%)', opacity: 0.5 }),
+            animate('500ms', style({ transform: 'translateY(0)', opacity: 1 }))
+        ])
+    ]
+);
