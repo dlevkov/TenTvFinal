495535dd918706afc3ef21bf02f316776ac4ebaa
Denis Levkov <denisl@nana10.tv>
2017-02-28 08:32:49 +0200

    Twitter NanaRoute init

--- src/app/common/components/twitter/twitter.component.ts
+++ src/app/common/components/twitter/twitter.component.ts
@@ -2,18 +2,21 @@ import {
     INanaRouteProvider,
     NanaRouterDataSenderService
 } from '../../../targeted/services/nanaRouter-data-sender.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'twitter',
     templateUrl: 'twitter.component.html'
 })
-export class TwitterComponent implements INanaRouteProvider {
+export class TwitterComponent implements INanaRouteProvider, OnInit {
     constructor(private nanaRouter: NanaRouterDataSenderService) {
 
     }
 
     public SendData() {
         this.nanaRouter.SendTwitter();
+    }
+    public ngOnInit(){
+        this.SendData();
     }
 }
